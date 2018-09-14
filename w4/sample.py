c28e2e57e9e8a7780d22cc5618bac37333ae2371
Weijia Li <wli21@ncsu.edu>
2018-09-13 23:56:00 -0400

    w4

---
+++ w4/sample.py
@@ -0,0 +1,55 @@
+"""
+Rewrite from sample.luo
+list index in luo start from 1
+
+@author: Weijia Li
+"""
+
+from __future__ import division
+from testEngine import O
+import random, math
+
+class Sample:
+
+    def __init__(self,max_sample = 128):
+        self.max_sample = max_sample
+        self.n = 0
+        self.sorted = False
+        self.some = []
+
+    def sampleInc(self,x):
+        self.n += 1
+        now = len(self.some)
+        if now < self.max_sample:
+            self.sorted = False
+            self.some.append(x)
+        elif random.random() < now/self.n:
+            self.sorted = False
+            self.some[math.floor(random.random()*now)] = x
+        return x
+
+    def sampleSorted(self):
+        if not self.sorted:
+            self.sorted = True
+            self.some.sort()
+        return self.some
+
+    def nth(self,n):
+        s = self.sampleSorted()
+        return s[min(len(s),max(0,math.floor(len(s)*n)))]
+
+
+"""test
+@O.k
+def testSample():
+    random.seed(1)
+    s = [ Sample(2**i) for i in range(5,11)]
+    for i in range(10000):
+        y = random.random()
+        for j,t in enumerate(s):
+            t.sampleInc(y)
+
+    for _,t in enumerate(s):
+        print(t.max_sample, t.nth( 0.5))
+        assert abs(t.nth(0.5)-0.5) < 0.2
+"""
---
+++ w4/sym.py
@@ -0,0 +1,54 @@
+"""
+Rewrite from sym.luo
+
+@author: Weijia Li
+"""
+from __future__ import division
+from collections import defaultdict
+from testEngine import O
+import random, math
+
+class Sym:
+
+    def same(x): return x
+    
+    def __init__(self,inits=[],f=same):
+        self.counts = defaultdict(int)
+        self.mode = None
+        self.most = 0
+        self.n = 0
+        self._ent = None
+        [self.symInc(x) for x in inits]
+
+    def symInc(self,x):
+        if x is "?": return x
+        self._ent = None
+        self.n += 1
+        self.counts[x] += 1
+        if self.counts[x] > self.most:
+            self.most = self.counts[x]
+            self.mode = x
+        return x
+
+
+    def symDec(self,x):
+        self._ent = None
+        self.n -= 1
+        self.counts[x] -= 1
+        return x
+
+    def symEnt(self):
+        if self._ent is None:
+            self._ent = 0
+            for _,n in self.counts.items():
+                p = n/self.n
+                self._ent -= p * math.log(p,2)
+        return self._ent
+
+"""test
+@O.k
+def testSym():
+    s = Sym(['y','y','y','y','y','y','y','y','y','n','n','n','n','n'])
+    assert abs(s.symEnt() - 0.9403) < 0.01
+    print(s.symEnt())
+"""
