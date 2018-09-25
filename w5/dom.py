422d53736c73f10a44887062e88e1c1220db8a40
Weijia Li <wli21@ncsu.edu>
2018-09-24 21:09:41 -0400

    halfway through

---
+++ w5/dom.py
@@ -0,0 +1,71 @@
+from __future__ import division
+from collections import defaultdict
+from lib.testEngine import O
+from lib.rows import rows
+import random, math, re, sys, operator
+
+samples = 100
+
+def dump(a,sep = "\t"):
+	for i in a:
+		i[len(i)-1] = str("%.2f" % i[len(i)-1])
+		print(sep.join(str(e) for e in i))
+
+def cap(x, lo, hi):  return min(hi, max(lo, x))
+
+def another(x,t):
+	y = cap(math.floor(random.random()*len(t)),0,len(t)-1)
+	if x==y:
+		return another(x,t)
+	if t[y]:
+		return t[y]
+	return another(x,t)
+
+def dom(data,row1,row2):
+	s1,s2 = 0,0
+	n = len(data.w)
+	for c,w in data.w.items():
+		a0  = row1[c]
+		b0  = row2[c]
+		a   = data.nums[c].numNorm(a0)
+		b   = data.nums[c].numNorm(b0)
+		s1 -= 10**(w * (a-b)/n)
+		s2 -= 10**(w * (b-a)/n)
+	return s1/n < s2/n
+
+def doms(data,brief):
+	if len(data.w) == 0: return
+	n = samples
+	c = len(data.name) 
+	print("\t".join(data.name) + "\t>dom")
+	for r1,row1 in enumerate(data.rows):
+		row1.append(0)
+		for i in range(n):
+			row2 = another(r1,data.rows) 
+			s = dom(data,row1,row2) and 1/n or 0
+			row1[c] += s
+
+	"""show only first and last 10 lines sorted by dom scroe from highes to lowest"""
+	o = len(data.rows) - 20
+	if brief and o > 0:
+		sorted(data.rows, key=operator.itemgetter(len(data.rows[0])-1))
+		dump(data.rows[:10])
+		print("---------------- Omitted "+ str(o) +" Lines -----------------")
+		dump(data.rows[-10:])
+	else:
+		dump(data.rows)
+
+
+def mainDom(s,brief = False):
+	doms(rows(s),brief)
+
+@O.k
+def testDom1():
+	random.seed(1)
+	print("\nweatherLong.csv\n")
+	mainDom("data/weatherLong.csv")
+
+@O.k
+def testDom2():
+	print("\nauto.csv\n")
+	mainDom("data/auto.csv",True)
---
+++ w5/lib/rows.py
@@ -0,0 +1,103 @@
+from __future__ import division
+from collections import defaultdict
+from lib.testEngine import O
+from lib.num import Num
+from lib.sym import Sym
+import random, math, re, sys
+
+class Data:
+
+    def __init__(self):
+        self.w = {}
+        self.syms = {}
+        self.nums = {}
+        self._class = None
+        self.rows = []
+        self.name = []
+        self._use = []
+        self.indeps = []
+
+    '''def indep(self,c): return not self.w[c] and self.calss != c'''
+    def indep(self,c): return not c in self.w and self.calss != c
+
+    def dep(self,c): return not indep
+
+    def header(self,cells):
+        for c0,x in enumerate(cells):
+            if not "?" in x:
+                c = len(self._use)
+                self._use.append(c0)
+                self.name.append(x)
+                if re.search("[<>$]", x):
+                    self.nums[c] = Num()
+                else: self.syms[c] = Sym()
+                if re.search("<", x): self.w[c]  = -1
+                elif re.search(">", x): self.w[c]  =  1
+                elif re.search("!", x): self._class =  c
+                else: self.indeps.append(c)
+        return self
+        
+    def row(self,cells):
+        l = len(self.rows)
+        self.rows.append([])
+        for c,c0 in enumerate(self._use):
+            x = cells[c0]
+            if x is not "?":
+                if c in self.nums:
+                    try: x = int(x)
+                    except ValueError:
+                        x= float(x)
+                    self.nums[c].numInc(x)
+                else:
+                    self.syms[c].symInc(x)
+            self.rows[l].append(x)
+        return self
+"""-------------end of calss data----------------"""
+
+"""taken from data.py"""
+def lines(s=None):
+   "Return contents, one line at a time."
+   if not s:
+     for line in sys.stdin:
+       yield line
+   elif s[-3:] in ["csv","dat"]:
+     with open(s) as fs:
+       for line in fs: 
+         yield line
+   else:
+     for line in s.splitlines():
+       yield line
+
+def rows1(src):
+    data = Data()
+    first = True
+    for line in src:
+        line = re.sub(r'([ \n\r\t]|#.*)', "", line)
+        cells = line.split(",")
+        if len(cells) > 0 and cells[0] != "":
+            if first: data.header(cells)
+            else: data.row(cells)
+            first = False
+    """       
+    print("\t\t\tn\tmode\tfrequency\n")
+    for key,val in data.syms.items():
+        print(f'{key}\t{data.name[key]}\t\t{val.n}\t{val.mode}\t{val.most}')
+    print("\n\t\t\tn\tmu\tsd\n")
+    for key,val in data.nums.items():
+        print(f'{key}\t{data.name[key]}\t\t{val.n}\t{val.mu:.2f}\t{val.sd:.2f}')
+    """
+    return data
+
+
+def rows(s):
+    return rows1(lines(s))
+
+"""@O.k
+def testRows():
+    print("\nweather.csv\n")
+    rows("weather.csv")
+    print("\nweatherLong.csv\n")
+    rows("weatherLong.csv")
+    print("\nauto.csv\n")
+    rows("auto.csv")
+"""
